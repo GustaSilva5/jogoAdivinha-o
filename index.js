323bc084b43646344805ece02944079e4864132f
GustaSilva5 <79280794+GustaSilva5@users.noreply.github.com>
2025-01-31 21:07:36 -0300

    Update index.js
    refaturação de codigo Javascript

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+//Variaveis
 const screen1 =  document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
 const randonNumber = Math.round(Math.random() * 10);
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
 let xAttempts = 1;
+
+
+// Eventos 
+btnTry.addEventListener('click', handleTryClick);
+btnReset.addEventListener('click', handleResetClick)
 
 
 //função callback
@@ -11,25 +19,20 @@ function handleTryClick(event){
     const inputNumber = document.querySelector("#inputNumber");
 
     if(Number(inputNumber.value) == randonNumber){
-        screen1.classList.add("hide")
-        screen2.classList.remove("hide")
-
-        document
-        .querySelector(".screen2 h2")
-        .innerText = `Voce acertou em ${xAttempts} tentativas`
+        toggleScreen()
+        screen2.querySelector("h2").innerText = `Voce acertou em ${xAttempts} tentativas`
     }
 
     inputNumber.value = ""
     xAttempts++
 }
 
-// Eventos 
-const btnTry = document.querySelector("#btnTry");
-const btnReset = document.querySelector("#btnReset");
-
-btnTry.addEventListener('click', handleTryClick);
-btnReset.addEventListener('click', function() {
-    screen1.classList.remove("hide");
-    screen2.classList.add("hide");
+function handleResetClick() {
+    toggleScreen()
     xAttempts = 1;
-})
+}
+
+function toggleScreen() {
+    screen1.classList.toggle("hide");
+    screen2.classList.toggle("hide");
+}
